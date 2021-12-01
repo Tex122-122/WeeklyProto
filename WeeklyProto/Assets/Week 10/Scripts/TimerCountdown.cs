372a8e107b5a19e736f0e3908414e1535c8c96fd
Brandon <1020232@student.sae.edu.au>
2021-12-01 17:25:48 +1100

    scene

--- WeeklyProto/Assets/Week 10/Scripts/TimerCountdown.cs
+++ WeeklyProto/Assets/Week 10/Scripts/TimerCountdown.cs
@@ -1,1 +1,25 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
+public class TimerCountdown : MonoBehaviour
+{
+    public string LevelToLoad;
+    private float timer = 60;
+    private Text timerSeconds;
+
+    void Start()
+    {
+        timerSeconds = GetComponent<Text>();  
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        timerSeconds.text = timer.ToString("f2");
+        if (timer <= 0)
+        {
+            Application.LoadLevel(LevelToLoad);
+        }
+    }
+}
